687e724f677705e1fc3b04bda91bece608d6cf33
Alexander Mock <amock@uos.de>
2024-02-14 17:16:43 +0100

    added new

--- ceres_localization/launch/slam_toolbox_launch.py
+++ ceres_localization/launch/slam_toolbox_launch.py
@@ -17,11 +17,35 @@ def generate_launch_description():
         description='Use simulation/Gazebo clock')
     declare_slam_params_file_cmd = DeclareLaunchArgument(
         'slam_params_file',
-        default_value=os.path.join(get_package_share_directory("ceres_localization"),
-                                   'config', 'slam_toolbox.yaml'),
+        default_value=os.path.join(get_package_share_directory("ceres_localization"),'config', 'slam_toolbox.yaml'),
         description='Full path to the ROS2 parameters file to use for the slam_toolbox node')
 
-    start_async_slam_toolbox_node = Node(
+    # what does this do?
+    map_saver_server = Node(
+            package='nav2_map_server',
+            executable='map_saver_server',
+            name='map_saver_server',
+            output='screen',
+            respawn=True,
+            respawn_delay=2.0,
+            arguments=['--ros-args', '--log-level', 'INFO'],
+            parameters=[
+                slam_params_file,
+                {'use_sim_time': use_sim_time}
+            ])
+    
+    life = Node(
+            package='nav2_lifecycle_manager',
+            executable='lifecycle_manager',
+            name='map_saver_server_life',
+            output='screen',
+            arguments=['--ros-args', '--log-level', "INFO"],
+            parameters=[{'use_sim_time': use_sim_time},
+                        {'autostart': True},
+                        {'node_names': ['map_saver_server']}])
+
+
+    slam = Node(
         parameters=[
           slam_params_file,
           {'use_sim_time': use_sim_time}
@@ -30,11 +54,18 @@ def generate_launch_description():
         executable='async_slam_toolbox_node',
         name='slam_toolbox',
         output='screen')
+    
+    
 
     ld = LaunchDescription()
 
     ld.add_action(declare_use_sim_time_argument)
     ld.add_action(declare_slam_params_file_cmd)
-    ld.add_action(start_async_slam_toolbox_node)
+    ld.add_action(slam)
+    
+    ld.add_action(map_saver_server)
+    ld.add_action(life)
+    
+
 
     return ld

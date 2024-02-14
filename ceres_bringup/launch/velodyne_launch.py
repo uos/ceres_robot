c7d0d72ccbf4af65b764b3c76e2aeed4dfc709d2
Isaak Ihorst <imitschke@uos.de>
2024-02-14 13:39:02 +0100

    added velodyne to laser

--- ceres_bringup/launch/velodyne_launch.py
+++ ceres_bringup/launch/velodyne_launch.py
@@ -5,8 +5,9 @@ import os
 import ament_index_python.packages
 
 from launch import LaunchDescription
-from launch_ros.actions import ComposableNodeContainer
+from launch_ros.actions import ComposableNodeContainer, Node
 from launch_ros.descriptions import ComposableNode
+from launch.substitutions import LaunchConfiguration
 import sys
 import yaml
 
@@ -57,4 +58,25 @@ def generate_launch_description():
             output='both',
     )
 
-    return LaunchDescription([container])
+    laser2pcd = Node(
+            package='pointcloud_to_laserscan', executable='pointcloud_to_laserscan_node',
+            remappings=[('cloud_in', [LaunchConfiguration(variable_name='scanner'), '/velodyne/points']),
+                        ('scan', [LaunchConfiguration(variable_name='scanner'), '/velodyne/multi_scan'])],
+            parameters=[{
+                'target_frame' : 'base_footprint',
+                'transform_tolerance': 0.01,
+                'min_height': 0.1,
+                'max_height': 1.0,
+                'angle_min': -1.5708,  # -M_PI/2
+                'angle_max': 1.5708,  # M_PI/2
+                'angle_increment': 0.0087,  # M_PI/360.0
+                'scan_time': 0.3333,
+                'range_min': 0.45,
+                'range_max': 40.0,
+                'use_inf': True,
+                'inf_epsilon': 1.0
+            }],
+            name='pointcloud_to_laserscan'
+        )
+
+    return LaunchDescription([container, laser2pcd])

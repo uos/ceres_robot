4be6a6e11ed26d86fdc4496e275c59c2ad5c4359
Alexander Mock <amock@uos.de>
2025-05-19 19:20:16 +0200

    removed unused files

--- ceres_navigation/launch/nav2_launch.py
+++ ceres_navigation/launch/nav2_launch.py
@@ -30,9 +30,6 @@ def generate_launch_description():
     # Get the launch directory
     bringup_dir = get_package_share_directory('ceres_navigation')
 
-
-   
-
     namespace = LaunchConfiguration('namespace')
     use_sim_time = LaunchConfiguration('use_sim_time')
     autostart = LaunchConfiguration('autostart')
